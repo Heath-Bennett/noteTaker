9edbe93ca4f81bfa0b61a92cad02e18aa012dc8e
Heath Bennett <heathben418@gmail.com>
2021-03-06 13:20:57 -0500

    created htmlRoute.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/', (_, res) => {
+    res.sendFile(path.join(_dirname, '..', 'public','index.thml'));
+});
+
+router.get('/notes', (_, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'notes.html'));
+
+});
+
+router.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('routes/htmlRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, ('public')));
+
+
+app.use('/', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
